a16029f0c4a3d79243f7c151c8c92c9e2962b7a0
V.A.K.M.Nethmina <cst21022@std.uwu.ac.lk>
2025-06-18 14:01:20 +0530

    Update

--- src/Pages/Admin/ReefTourdetails.jsx
+++ src/Pages/Admin/ReefTourdetails.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Navbar from '../../Components/Navbar/Navbar'
+import AdminNavbar from '../../Components/Navbar/AdminNavbar'
+
 
 function ReefTourdetails() {
   return (
     <div>
-        <h1>This is reef tour details page</h1>
+       <Navbar />
+       <div className="flex min-h-screen bg-[#eaf4f6]">
+         <div className="w-60 bg-white shadow-md mt-10">
+        <AdminNavbar />
+         </div>
+         {/* main content */}
+         <div>
+          
+
+         </div>
+
+       </div>
     </div>
   )
 }
